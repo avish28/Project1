08bf55287595fb15073d5008cc4c6ad0281ae945
avish28 <modiavish0@gmail.com>
2020-11-18 16:22:02 +0530

    student_details master table is created

--- classes/classes/settings.py
+++ classes/classes/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'course',
+    'course','student_details',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
